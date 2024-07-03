771f7ff669eb6f3594e715e6cd7b1a3ea3a7b225
qalqa <andreybas04@gmail.com>
2024-07-03 15:39:18 +0300

    test

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let template = document.getElementById("custom-paragraph");
-let templateContent = template.content;
-function templateReveal() {
-  document.body.appendChild(templateContent);
-}
+customElements.define(
+  "my-paragraph",
+  class extends HTMLElement {
+    constructor() {
+      super();
+      let template = document.getElementById("custom-paragraph");
+      let templateContent = template.content;
+
+      const shadowRoot = this.attachShadow({ mode: "open" });
+      shadowRoot.appendChild(templateContent.cloneNode(true));
+    }
+  }
+);
