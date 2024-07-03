02ef478a7cff9ea30da7c2e67a394dc5e587d26c
qalqa <andreybas04@gmail.com>
2024-07-03 15:49:03 +0300

    some test web components

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ customElements.define(
     }
   }
 );
+
+customElements.define(
+  "element-details",
+  class extends HTMLElement {
+    constructor() {
+      super();
+      const template = document.getElementById(
+        "element-details-template"
+      ).content;
+      const shadowRoot = this.attachShadow({ mode: "open" });
+      shadowRoot.appendChild(template.cloneNode(true));
+    }
+  }
+);
